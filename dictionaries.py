8cc196c495ca22bb33cd082cfae82e39b3bf62fe
Admin <admin@Admins-MacBook-Pro.local>
2017-10-05 22:01:54 -0700

    add dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,5 @@
+stuff = {"name": "Jason", "age": "24", "city": "San Jose", "language": "English"} # dictionary declaration
+print "My name is " + stuff["name"] # print string with key values
+print "My age is " + stuff["age"] # print string with key values
+print "My city of birth is " + stuff["city"] # print string with key values
+print "my favorite language is " + stuff["language"] # print string with key values
